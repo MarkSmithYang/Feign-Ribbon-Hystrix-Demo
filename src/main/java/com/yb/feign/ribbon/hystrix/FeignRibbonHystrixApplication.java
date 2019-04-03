534f9fc9993cded69dcd15cad8e91cc0e7c58f2b
yangbiao <itbiaoyang@163.com>
2019-04-03 17:57:43 +0800

    提交1

--- src/main/java/com/yb/feign/ribbon/hystrix/FeignRibbonHystrixApplication.java
+++ src/main/java/com/yb/feign/ribbon/hystrix/FeignRibbonHystrixApplication.java
@@ -3,11 +3,13 @@ package com.yb.feign.ribbon.hystrix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableHystrix
 @EnableFeignClients
 @EnableHystrixDashboard
 @SpringCloudApplication
--- src/main/java/com/yb/feign/ribbon/hystrix/controller/FeignRibbonController.java
+++ src/main/java/com/yb/feign/ribbon/hystrix/controller/FeignRibbonController.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 /**
- * Description:
+ * Description:注意使用熔断器的仪表盘查看相关配置
+ * 因为是服务间的调用需要熔断,所以才会有feign和ribbon的调用熔断,网关进来是直接路由到指定的服务,
+ * 如果没有在服务里调用其他的服务,那么就不需要熔断,需要熔断都是服务调用服务的情况,解决调用服务的异常而导致自己的服务也异常了
+ * 如果自己的没有调用其他的服务,除了异常了就直接报异常了,根本不会传递异常给谁,除非别的服务调用你,又没有做熔断处理
  * author biaoyang
  * date 2019/4/3 000316:03
  */
@@ -28,6 +31,7 @@ public class FeignRibbonController {
     }
 
     /**
+     * 注意使用熔断器的仪表盘查看相关配置
      * 这种需要写接口的方式就是feign了,而且feign还需要在配置文件类设置开启熔断才行(实测),可以看出ribbon简便很多
      * @param name
      * @return
@@ -38,6 +42,7 @@ public class FeignRibbonController {
     }
 
     /**
+     * 注意使用熔断器的仪表盘查看相关配置
      * 直接使用注解注定回调熔断的方法,这种就适合ribbon
      * @return
      */
@@ -49,6 +54,7 @@ public class FeignRibbonController {
     }
 
     /**
+     * 注意使用熔断器的仪表盘查看相关配置
      * 熔断调用方法
      * @return
      */
